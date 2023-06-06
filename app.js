6bc360ba61195c7d068846247f46ad3b307fdadc
Savy011 <106910589+Savy011@users.noreply.github.com>
2023-06-06 17:18:23 +0530

    Finished Ex 11.11

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
+
+app.get('/health', (_req, res) => {
+
+	res.status(200).send('ok')
+})
 
 app.listen(PORT, () => {
   console.log('server started on port 3000')
